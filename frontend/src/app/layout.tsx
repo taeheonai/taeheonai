e55a64c8c6e13d855c0ecbdc5097bb8829d02496
taeheonai <oheth9@gmail.com>
2025-08-12 17:09:44 +0900

    0812

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/api/manifest" />
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
