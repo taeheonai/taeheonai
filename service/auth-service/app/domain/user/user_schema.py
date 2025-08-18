896c40af93b9460c5ed799757dcdc0ea73fd2553
taeheonai <oheth9@gmail.com>
2025-08-18 10:18:36 +0900

    0818

--- service/auth-service/app/domain/user/user_schema.py
+++ service/auth-service/app/domain/user/user_schema.py
@@ -5,11 +5,11 @@ from typing import Optional
 class SignupIn(BaseModel):
     # id는 보통 DB에서 자동발급. 필요시 Optional로 허용
     id: Optional[int] = Field(default=None)
-    company_id: Optional[str] = None
+    company_id: Optional[int] = None  # str -> int로 변경
     industry: Optional[str] = None
     email: Optional[str] = None
     name: Optional[str] = None
-    birth: Optional[str] = None  # age에서 birth로 변경
+    birth: Optional[str] = None  # birth 필드명 유지
     auth_id: str = Field(..., min_length=3, max_length=64)
     auth_pw: str = Field(..., min_length=4, max_length=128)
 
