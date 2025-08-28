29c8282d0f26cff1895aa8f4f371c3be2862dd71
taeheonai <oheth9@gmail.com>
2025-08-28 11:36:24 +0900

    0828

--- gateway/app/main.py
+++ gateway/app/main.py
@@ -45,6 +45,7 @@ if IS_PROD or IS_RAILWAY:
     ALLOW_ORIGINS = [
         "https://taeheonai.com",
         "https://www.taeheonai.com",
+        "https://front-six-teal.vercel.app/"
     ]
     # vercel/railway 서브도메인을 모두 허용해야 할 때만 사용
     ALLOW_ORIGIN_REGEX = r"^https://([a-z0-9-]+\.)?(vercel\.app|railway\.app)$"
