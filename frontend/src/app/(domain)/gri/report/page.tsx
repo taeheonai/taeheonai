8ccfa5a873f879067bd1f0b5da54a42c72aeb8c6
taeheonai <oheth9@gmail.com>
2025-08-28 18:31:08 +0900

    0828 put llm

--- frontend/src/app/(domain)/gri/report/page.tsx
+++ frontend/src/app/(domain)/gri/report/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React from 'react';
 import { useEffect, useState } from 'react';
 import Navigation from '@/components/Navigation';
 import ProtectedRoute from '@/components/ProtectedRoute';
