3ebecd3df18312fa276fc632487adb46f13cc3d8
taeheonai <oheth9@gmail.com>
2025-09-02 16:29:14 +0900

    0902 report second commit

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { useRouter, usePathname } from 'next/navigation';
 
-import { User } from '@/contexts/AuthContext';
+import type { User } from '@/contexts/AuthContext';
 
 interface NavigationProps {
   user?: User | null;
